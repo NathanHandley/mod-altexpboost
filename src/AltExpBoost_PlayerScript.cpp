7a9b9f9c9559f59953f9b052c9ec4e85df93b21b
NathanHandley <nathanhandley@protonmail.com>
2025-08-10 18:35:25 -0500

    Set default maximum

--- src/AltExpBoost_PlayerScript.cpp
+++ src/AltExpBoost_PlayerScript.cpp
@@ -46,7 +46,6 @@ public:
     {
         if (AltExpBoost->IsEnabled == false)
             return;
-        int numOfAffectingChar = AltExpBoost->GetNumOfInfluencingCharHigherThanLoggedInChar(player);
         AltExpBoost->AnnounceCurrentBonus(player);
     }
 
